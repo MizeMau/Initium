f262630e8cf11fb777c01d41b8847c1f0873da3c
MizeMau <johannes.zielenbach@outlook.de>
2025-11-29 18:02:44 +0100

    Add calendar

--- Frontend/src/components/home/calendar.vue
+++ Frontend/src/components/home/calendar.vue
@@ -1,22 +1,100 @@
 ﻿<template>
-  <div>
-    test
+  <div class="mt-5">
+    <div v-if="!selectedMonth"
+         class="row">
+      <div v-for="month in calendar"
+           class="col-xl-3 my-3">
+        <div class="card text-center"
+             role="button"
+             v-on:click="selectedMonth = month">
+          <h3 class="card-header">{{month.name}}</h3>
+          <div class="card-body">
+            <table class="table table-sm">
+              <thead>
+                <tr>
+                  <th>Mo.</th>
+                  <th>Tu.</th>
+                  <th>We.</th>
+                  <th>Th.</th>
+                  <th>Fr.</th>
+                  <th>Sa.</th>
+                  <th>Su.</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="week in month.weekCount">
+                  <template v-if="week == 1">
+                    <td v-for="offset in month.weekdayOffset"
+                        />
+                  </template>
+                  <td v-for="day in getWeekDaysByMonthAndWeek(month, week - 1)"
+                      v-bind:class="day.isToday ? 'bg-primary' : ''">
+                    {{day.number}}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <month-component v-else 
+                     v-bind:month="selectedMonth" 
+                     v-on:exit="selectedMonth = null" />
   </div>
 </template>
 
 <script>
+  import MonthComponent from './month.vue'
+
   export default {
     name: "Calendar",
+    components: {
+      'month-component': MonthComponent
+    },
     data() {
       return {
-
+        selectedYear: new Date().getFullYear(),
+        calendar: {},
+        selectedMonth: null,
       };
     },
     mounted() {
+      var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      for (let i = 0; i < 12; i++) {
+        let month = {
+          name: monthNames[i],
+          weekCount: 0,
+          weekdayOffset: 0,
+          days: [],
+        }
+        const dateFirst = new Date(this.selectedYear, i, 1)
+        const dateLast = new Date(this.selectedYear, i + 1, 0)
+        const monthDays = dateLast.getDate();
+        let weekdayIndex = dateFirst.getDay();
+        month.weekdayOffset = weekdayIndex != 0 ? weekdayIndex - 1 : 6
 
+        for (let y = 0; y < monthDays; y++) {
+          month.days.push({
+            number: y + 1
+          })
+          if (y % 7 == 0) month.weekCount++
+        }
+        this.calendar[i] = month
+      }
+
+      const today = new Date()
+      const monthIndex = today.getMonth()
+      const dayIndex = today.getDate() - 1
+      this.calendar[monthIndex].days[dayIndex].isToday = true
     },
     methods: {
-
+      getWeekDaysByMonthAndWeek(month, week) {
+        const index = week * 7 - month.weekdayOffset
+        const startIndex = week != 0 ? index : 0
+        const endIndex = index + 7
+        return month.days.slice(startIndex, endIndex)
+      }
     },
-  };
+  }
 </script>
---
+++ Frontend/src/components/home/month.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="card text-center">
+        <h3 class="card-header">
+          {{month.name}}
+        </h3>
+        <div class="card-body">
+          <table class="table mt-2">
+            <thead>
+              <tr>
+                <th class="bg-transparent">Mo.</th>
+                <th class="bg-transparent">Tu.</th>
+                <th class="bg-transparent">We.</th>
+                <th class="bg-transparent">Th.</th>
+                <th class="bg-transparent">Fr.</th>
+                <th class="bg-transparent">Sa.</th>
+                <th class="bg-transparent">Su.</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="week in month.weekCount">
+                <template v-if="week == 1">
+                  <td v-for="offset in month.weekdayOffset"
+                      class="bg-transparent" />
+                </template>
+                <td v-for="day in getWeekDaysByMonthAndWeek(month, week - 1)"
+                    v-bind:class="day.isToday ? 'bg-primary' : ''">
+                  {{day.number}}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <button class="btn btn-secondary mt-4"
+              v-on:click="emitExit">
+        Back
+      </button>
+    </div>
+    <div class="col-md-6">
+      <div class="card">
+        <h3 class="card-header">
+          Tasks
+        </h3>
+        <div class="card-body">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Month",
+    props: ['month'],
+    emits: ['exit'],
+    data() {
+      return {
+
+      };
+    },
+    mounted() {
+
+    },
+    methods: {
+      getWeekDaysByMonthAndWeek(month, week) {
+        const index = week * 7 - month.weekdayOffset
+        const startIndex = week != 0 ? index : 0
+        const endIndex = index + 7
+        return month.days.slice(startIndex, endIndex)
+      },
+      emitExit() {
+        this.$emit('exit')
+      },
+    },
+  }
+</script>
